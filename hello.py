3506e4df0b9530c1ea7ed6e1f4ce7ea31e0dd131
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 14:42:08 +0900

    함수 더 알아보기 - 매개변수의 기본값 설정

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-def sub(a, b):
-    print(a - b)
+def total(a, b=5, c=10):
+    print(a + b + c)
 
-sub(1, 2)
-sub(a=1, b=2)
-sub(b=1, a=2)
+total(1)
+total(1, 2)
+total(1, 2, 3)
+# total(b=2, c=3) # 기본값을 설정하지 않은 매개변수에 인수를 전달하지 않음 -> TypeError
+# total(1, 2, 3, 4) # 매개변수의 수보다 더 많은 인수를 전달함 -> TypeError
