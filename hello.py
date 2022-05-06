b07a67da3121d9c56d1818a62062f5be5cc209b9
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 11:20:21 +0900

    turtle 모듈 - 사각형 그리기

--- hello.py
+++ hello.py
@@ -1,15 +1,11 @@
-import math
+from turtle import *
 
-print(math.pi)
-print(math.e)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
 
-radius = 5.0
-area = (radius ** 2) * math.pi
-theta = math.radians(60)
-
-x = radius * math.cos(theta)
-y = radius * math.sin(theta)
-
-print("area: " + str(area))
-print("x: " + str(x))
-print("y: " + str(y))
+mainloop()
