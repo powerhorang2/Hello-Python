18bf69c014cf81ecf86b8612fcadad5069145d6e
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 14:43:34 +0900

    함수 더 알아보기 - 매개변수의 기본값 설정 시 주의 점

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-def total(a, b=5, c=10):
+def total(a=5, b, c=10):
     print(a + b + c)
 
-total(1)
-total(1, 2)
-total(1, 2, 3)
-# total(b=2, c=3) # 기본값을 설정하지 않은 매개변수에 인수를 전달하지 않음 -> TypeError
-# total(1, 2, 3, 4) # 매개변수의 수보다 더 많은 인수를 전달함 -> TypeError
+
+total(1) # 오류 발생 매개변수 b 에 값이 들어가지 않고 a 에 값이 들어감 b 에는 기본값이 없기 때문에 SyntaxError 발생
+# 이처럼 매개변수의 기본값 설정은 매개변수의 목록에서 항상 오른쪽부터 차례대로 명시되어야만 합니다.
