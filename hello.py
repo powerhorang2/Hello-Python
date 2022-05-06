a7b0115ad23c0ad4cc2039a91c9b3279590044b5
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 10:58:24 +0900

    모듈 사용법 2 - 모듈의 함수만 import

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-import classNmodule
+from classNmodule import Horse
 
-hihi = classNmodule.Horse(5,160,'brown',0,10)
-bow = classNmodule.Dog(5,100,'brown',10,10)
+hihi = Horse(5,160,'brown',0,10)
+bow = Dog(5,100,'brown',10,10)
 
 hihi.sound()
 bow.sound()
