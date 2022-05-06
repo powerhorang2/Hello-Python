a8d8d465dedec7825866cb5896f0f364f5c4c6f5
powerhorang2 <tjrdbqls3@naver.com>
2022-05-06 10:57:42 +0900

    모듈 사용법 1 - 모듈 자체 import

--- hello.py
+++ hello.py
@@ -1,60 +1,7 @@
-import math
+import classNmodule
 
+hihi = classNmodule.Horse(5,160,'brown',0,10)
+bow = classNmodule.Dog(5,100,'brown',10,10)
 
-class Animal:
-
-    def __init__(self, age, height, color, xpos, ypos):
-        self.age = age
-
-        self.height = height
-
-        self.color = color
-
-        self.xposition = xpos
-
-        self.yposition = ypos
-
-        self.velocity = 0
-
-    def sound(self):
-        pass
-
-
-class Horse(Animal):
-
-    def __init__(self, age, height, color, xpos, ypos):
-        Animal.__init__(self, age, height, color, xpos, ypos)
-
-    def sound(self):
-        print('Neigh')
-
-    def run(self, xdistance, ydistance, time):
-        self.xposition += xdistance
-
-        self.yposition += ydistance
-
-        total_distance = math.sqrt((xdistance + xdistance) * (ydistance + ydistance))
-
-        self.velocity = total_distance / time
-
-
-class Dog(Animal):
-
-    def __init__(self, age, height, color, xpos, ypos):
-        Animal.__init__(self, age, height, color, xpos, ypos)
-
-    def sound(self):
-        print('Bow-Wow')
-
-
-if __name__ == '__main__':
-    danbi = Horse(5, 160, 'brown', 0, 0)
-
-    choco = Dog(10, 100, 'black', 50, 30)
-
-    danbi.sound()
-    danbi.run(10, 10, 1)
-    print(danbi.velocity)
-    print(danbi.xposition)
-    print(danbi.yposition)
-    choco.sound()
+hihi.sound()
+bow.sound()
