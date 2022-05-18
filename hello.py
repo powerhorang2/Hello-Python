9de1ce947516a264621faa0495b6d25363060d82
powerhorang2 <tjrdbqls3@naver.com>
2022-05-18 11:46:24 +0900

    리스트 기본 - 리스트 인덱스 호출 거꾸로

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 a = [1,2,3,4,5,6,7]
-print(a[0] + a[1])
-print(a[4] - a[1])
-print(a[1] * a[4])
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
+print(a[-5])
+print(a[-6])
+print(a[-7])
+print(a[-8]) # 인덱스 범위를 벗어나면 out of range 오류가 난다.
