3233da0f8724b8d2160ad6c793844748176112c6
powerhorang2 <tjrdbqls3@naver.com>
2022-05-18 11:45:05 +0900

    리스트 기본 - 리스트 인덱스 호출 후 연산

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 a = [1,2,3,4,5,6,7]
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
-print(a[4])
-print(a[5])
-print(a[6])
-print(a[7]) # 인덱스를 벗어나면 out of range 오류에 걸린다.
+print(a[0] + a[1])
+print(a[4] - a[1])
+print(a[1] * a[4])
