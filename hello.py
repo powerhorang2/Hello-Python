bb972dd6ddb110ff5439e848389471aeeb1f9d9d
powerhorang2 <tjrdbqls3@naver.com>
2022-05-18 11:19:07 +0900

    숫자 연산자 - 사칙 연산

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
-# 8진수는 0o
-# 16진수는 0x
-
-a = 0o320
-b = 0xf420
+a = 5 + 4 # 더하기
 print(a)
+
+b = a - 2 # 빼기
 print(b)
+
+c = b * 9 # 곱하기
+print(c)
+
+d = c / 6 # 나누기
+print(d)
+
+f = 30 % 9 # 나누기 - 나머지
+print(f)
+
+g = 30 // 9 # 나누기 - 몫
+print(g)
+
+h = 3**10 # 제곱
+print(h)
+
