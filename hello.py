bc316b7a7232fefbeff658b6e670635c71d44e60
powerhorang2 <tjrdbqls3@naver.com>
2022-05-21 10:33:21 +0900

    수행시간 출력 소스코드

--- hello.py
+++ hello.py
@@ -1,36 +1,7 @@
-# 그래프는 딕셔너리로 감싸고 그 안의 벨류 값을 리스트로 구현
-graph = {
-    1: [2, 3, 4],
-    2: [5],
-    3: [5],
-    4: [],
-    5: [6, 7],
-    6: [],
-    7: [3],
-}
+import time
 
+start_time = time.time()  # 측정 시작
 
-# 재귀 호출로 구현
-def recursive_dfs(v, visited=[]):
-    visited.append(v)  # 시작 정점 방문
-    for w in graph[v]:
-        if w not in visited:  # 방문 하지 않았으면
-            visited = recursive_dfs(w, visited)
-    return visited
-
-
-# 스택으로 구현
-def iterative_dfs(start_v):
-    visited = []
-    stack = [start_v]
-    while stack:
-        v = stack.pop()
-        if v not in visited:
-            visited.append(v)
-            for w in graph[v]:
-                stack.append(w)
-    return visited
-
-
-print("recursive_dfs: ", recursive_dfs(1))
-print("iterative_dfs: ", iterative_dfs(1))
+# 프로그램 소스코드
+end_time = time.time()  # 측정 종료
+print("time :", end_time - start_time)  # 수행 시간 출력
