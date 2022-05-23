2696c73c31084c854ce4be2ccc4aad12d0c5f4c6
powerhorang2 <tjrdbqls3@naver.com>
2022-05-23 18:07:17 +0900

    DFS(깊이 우선 탐색) 구현 - 재귀 호출, 스택

--- hello.py
+++ hello.py
@@ -1,9 +1,36 @@
-a = [1,2,3,4,5,6,7]
-print(a[-1])
-print(a[-2])
-print(a[-3])
-print(a[-4])
-print(a[-5])
-print(a[-6])
-print(a[-7])
-print(a[-8]) # 인덱스 범위를 벗어나면 out of range 오류가 난다.
+# 그래프는 딕셔너리로 감싸고 그 안의 벨류 값을 리스트로 구현
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3],
+}
+
+
+# 재귀 호출로 구현
+def recursive_dfs(v, visited=[]):
+    visited.append(v)  # 시작 정점 방문
+    for w in graph[v]:
+        if w not in visited:  # 방문 하지 않았으면
+            visited = recursive_dfs(w, visited)
+    return visited
+
+
+# 스택으로 구현
+def iterative_dfs(start_v):
+    visited = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in visited:
+            visited.append(v)
+            for w in graph[v]:
+                stack.append(w)
+    return visited
+
+
+print("recursive_dfs: ", recursive_dfs(1))
+print("iterative_dfs: ", iterative_dfs(1))
