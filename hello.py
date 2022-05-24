ae589a299a48ef23519c7bc926e1c427c0e10207
powerhorang2 <tjrdbqls3@naver.com>
2022-05-24 17:35:06 +0900

    그리디 - 큰 수의 법칙 - 강의 풀이

--- hello.py
+++ hello.py
@@ -1,32 +1,23 @@
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort()  # 입력받은 수들 정렬하기
+first = data[n - 1]  # 가장 큰 수
+second = data[n - 2]  # 두 번째로 큰 수
+
 result = 0
-line1 = '5 8 3'  # 입력 값을 받아야하는데 String 변수로 임의의 값을 지정함
-line2 = '2 4 5 4 6'  # 입력 값을 받아야하는데 String 변수로 임의의 값을 지정함
 
-array1 = line1.split(' ')
-array2 = line2.split(' ')
+while True:
+    for i in range(k):  # 가장 큰 수를 k번 더하기
+        if m == 0:  # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1  # 더할 때마다 1씩 빼기
+    if m == 0:  # m이 0이라면 반복문 탈출
+        break
+    result += second  # 두 번째로 큰 수를 한 번 더하기
+    m -= 1  # 더할 때마다 1씩 빼기
 
-n = int(array1[0])
-m = int(array1[1])
-k = int(array1[2])
-
-array2.sort()
-
-array = []
-for i in array2:
-    array.append(int(i))
-
-# map 과 int 함수를 사용해서 데이터를 추출하면 되는데 직접 타입변환함
-
-i = 0
-for _ in range(m):
-    if i < k:
-        result += array[n - 1]
-        i += 1
-    else:
-        result += array[n - 2]
-        i = 0
-# m 이라는 반복 기준이 있는데 i 변수를 생성해서 사용함
-# while 문을 사용해서 해결 가능함
-
-
-print("결과: ", result)
+print(result)  # 최종 답안 출력
