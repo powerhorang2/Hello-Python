d6707d11d704f59e3fc5c197b2e82581fd87b9c9
powerhorang2 <tjrdbqls3@naver.com>
2022-05-25 11:44:43 +0900

    그리디 - 숫자 카드 게임 - min() 함수를 이용하는 답안 예시

--- hello.py
+++ hello.py
@@ -1,18 +1,13 @@
-# N, M, K를 공백으로 구분하여 입력받기
-n, m, k = map(int, input().split())
-# N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int, input().split()))
-
-data.sort()  # 입력받은 수들 정렬하기
-first = data[n - 1]  # 가장 큰 수
-second = data[n - 2]  # 두 번째로 큰 수
-
-# 가장 큰 수가 더해지는 횟수 계산
-count = int(m / (k + 1)) * k
-count += m % (k + 1)
+# N, M을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
 
 result = 0
-result += count * first  # 가장 큰 수 더하기
-result += (m - count) * second  # 두 번째로 큰 수 더하기
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
 
-print("result: ", result)   # 최종 답안 출력
+print(result)  # 최종 답안 출력
