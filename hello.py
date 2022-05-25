8c1a2adf71988feb786408b6d457718a662af8af
powerhorang2 <tjrdbqls3@naver.com>
2022-05-25 17:39:34 +0900

    그리디 - 1이 될 때까지 - 답안 예시(이건 최선은 아닌듯??)

--- hello.py
+++ hello.py
@@ -1,19 +1,23 @@
+# N, K를 공백으로 구분하여 입력받기
 n, k = map(int, input().split())
 result = 0
 
-# N이 K 이상이라면 K로 계속 나누기
-while n >= k:
-    # N 이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
-    while n % k != 0:
-        n -= 1
-        result += 1
+while True:
+    # (N == k로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    print("n(N == k로 나누어 떨어지는 수)가 될 때까지 1씩 빼기): ", n)
+    print("result: ", result)
+    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
     # K로 나누기
-    n //= k
     result += 1
+    n //= k
+    print("n(K로 나누기): ", n)
+    print("result: ", result)
 
 # 마지막으로 남은 수에 대하여 1씩 빼기
-while n > 1:
-    n -= 1
-    result += 1
-
+result += (n - 1)
 print(result)
