d873cfde2dc77bda962be86c96c2ddf6030ae57c
powerhorang2 <tjrdbqls3@naver.com>
2022-05-25 14:57:50 +0900

    그리디 - 1이 될 때까지 - 내 풀이

--- hello.py
+++ hello.py
@@ -1,15 +1,24 @@
-# N, M을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
 result = 0
-# 한 줄씩 입력받아 확인
-for i in range(n):
-    data = list(map(int, input().split()))
-    # 현재 줄에서 '가장 작은 수' 찾기
-    min_value = 10001
-    for a in data:
-        min_value = min(min_value, a)
-    # '가장 작은 수'들 중에서 가장 큰 수 찾기
-    result = max(result, min_value)
 
-print("result: ", result)   # 최종 답안 출력
+while True:
+    # n 이 1이 되면 종료
+    if n == 1:
+        break
+    # n 이 k 로 나누어 떨어질 때
+    if n % k == 0:
+        n //= k
+        result += 1
+    # 나머지가 존재하면 나머지 만큼 n에서 빼기
+    else:
+        remainder = n % k  # 나머지 구함
+
+        # 만약 나눗셈의 몫이 0이면 나머지에 -1 한만큼 횟수 더해줌
+        if n // k == 0:
+            result += (remainder - 1)
+            break
+        n -= remainder
+        result += remainder
+
+print('result: ', result)
