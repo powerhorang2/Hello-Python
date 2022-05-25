b854eb32825e762abed61f55c775ba659683d588
powerhorang2 <tjrdbqls3@naver.com>
2022-05-25 11:18:49 +0900

    그리디 - 큰 수의 법칙 - 반복 수열 파악 후 가장 큰 수가 더해지는 횟수 이용해서 해결

--- hello.py
+++ hello.py
@@ -7,17 +7,12 @@ data.sort()  # 입력받은 수들 정렬하기
 first = data[n - 1]  # 가장 큰 수
 second = data[n - 2]  # 두 번째로 큰 수
 
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
 result = 0
+result += count * first  # 가장 큰 수 더하기
+result += (m - count) * second  # 두 번째로 큰 수 더하기
 
-while True:
-    for i in range(k):  # 가장 큰 수를 k번 더하기
-        if m == 0:  # m이 0이라면 반복문 탈출
-            break
-        result += first
-        m -= 1  # 더할 때마다 1씩 빼기
-    if m == 0:  # m이 0이라면 반복문 탈출
-        break
-    result += second  # 두 번째로 큰 수를 한 번 더하기
-    m -= 1  # 더할 때마다 1씩 빼기
-
-print(result)  # 최종 답안 출력
+print("result: ", result)   # 최종 답안 출력
