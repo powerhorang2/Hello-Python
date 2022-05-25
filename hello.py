a1580a03548e9a5ce39c4f5f4aaccedad4f6a5ba
powerhorang2 <tjrdbqls3@naver.com>
2022-05-25 11:51:52 +0900

    그리디 - 숫자 카드 게임 - 2중 반복문 구조를 이용하는 답안 예시

--- hello.py
+++ hello.py
@@ -6,8 +6,10 @@ result = 0
 for i in range(n):
     data = list(map(int, input().split()))
     # 현재 줄에서 '가장 작은 수' 찾기
-    min_value = min(data)
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
     # '가장 작은 수'들 중에서 가장 큰 수 찾기
     result = max(result, min_value)
 
-print(result)  # 최종 답안 출력
+print("result: ", result)   # 최종 답안 출력
