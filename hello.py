ebf5e96d342b89061ca712a495ec0eef33b09153
powerhorang2 <tjrdbqls3@naver.com>
2022-05-26 11:06:14 +0900

    구현 - 시뮬레이션(상하좌우) - 답안 예시

--- hello.py
+++ hello.py
@@ -1,19 +1,25 @@
-# N, K를 공백으로 구분하여 입력받기
-n, k = map(int, input().split())
-result = 0
+n = int(input())
+plans = input().split()
+# 현재 좌표
+x, y = 1, 1
 
-while True:
-    # (N == k로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
-    target = (n // k) * k
-    result += (n - target)
-    n = target
-    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
-    if n < k:
-        break
-    # K로 나누기
-    result += 1
-    n //= k
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
 
-# 마지막으로 남은 수에 대하여 1씩 빼기
-result += (n - 1)
-print(result)
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    nx, ny = 0, 0
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 실제로 좌표 이동
+    x, y = nx, ny
+
+print(x, y)
