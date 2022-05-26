b4ad094fb65fe846e72cd66f94b3df3a58f1389a
powerhorang2 <tjrdbqls3@naver.com>
2022-05-26 11:23:11 +0900

    구현 - 완전탐색(시각) - 답안 예시

--- hello.py
+++ hello.py
@@ -1,25 +1,10 @@
 n = int(input())
-plans = input().split()
-# 현재 좌표
-x, y = 1, 1
 
-# L, R, U, D에 따른 이동 방향
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_types = ['L', 'R', 'U', 'D']
+result = 0
+for i in range(n + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                result += 1
 
-# 이동 계획을 하나씩 확인
-for plan in plans:
-    nx, ny = 0, 0
-    # 이동 후 좌표 구하기
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    # 공간을 벗어나는 경우 무시
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    # 실제로 좌표 이동
-    x, y = nx, ny
-
-print(x, y)
+print(result)
