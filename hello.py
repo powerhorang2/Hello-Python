074d6e44bb3a12a160f2b90b7cad7989c5f066c7
powerhorang2 <tjrdbqls3@naver.com>
2022-05-26 12:02:11 +0900

    구현 - 시뮬레이션(왕실의 나이트) - 내 풀이

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
-n = int(input())
+pos = input()
+
+row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+x = row.index(pos[0]) + 1
+y = int(pos[1])
+
+steps = [(-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2)]
 
 result = 0
-for i in range(n + 1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                result += 1
+
+for step in steps:
+    nx = x + step[0]
+    ny = y + step[1]
+    if nx < 1 or ny < 1 or nx > 8 or ny > 8:
+        continue
+    result += 1
 
 print(result)
