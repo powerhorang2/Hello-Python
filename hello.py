96bd3e48ddf39708bf5deda649ba368f2ffe2824
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 15:28:48 +0900

    DFS_BFS - BFS - 예제

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
+from collections import deque
+
 # DFS 메서드 정의
-def dfs(graph, v, visited):
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
     # 현재 노드를 방문 처리
-    visited[v] = True
-    print(v, end=' ')
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
@@ -26,4 +35,4 @@ graph = [
 visited = [False] * 9
 
 # 정의된 DFS 함수 호출
-dfs(graph, 1, visited)
+bfs(graph, 1, visited)
