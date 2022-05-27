6bd2a35a42048bee46e94545f10a8622ef857181
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 16:49:02 +0900

    DFS_BFS - DFS - 음료수 얼려 먹기 - 예제(출력 추가)

--- hello.py
+++ hello.py
@@ -11,19 +11,21 @@ for i in range(n):
 def dfs(x, y):
     # 주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
+        print('(', x, ', ', y, ') 는 범위를 벗어남')
         return False
     # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
+        print('(',x,', ',y,') 노드 방문')
         # 해당 노드 방문 처리
-        graph[x][y] == 1
+        graph[x][y] = 1
         # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
         dfs(x - 1, y)
         dfs(x, y - 1)
         dfs(x + 1, y)
         dfs(x, y + 1)
         return True
+    print('(', x, ', ', y, ') 는 방문했던 노드')
     return False
-
 
 # 모든 노드(위치)에 대하여 음료수 채우기
 result = 0
@@ -31,6 +33,9 @@ for i in range(n):
     for j in range(m):
         # 현재 위치에서 DFS 수행
         if dfs(i, j) == True:
+            print('=====================')
+            print('음료수 채운 노드 좌표: (', i, ', ', j, ')',)
+            print('=====================')
             result += 1
 
 print(result)   # 정답 출력
