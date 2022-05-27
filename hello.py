b20ee4d5ff0ed397b13a9aac0ab23fd8affff4f4
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 13:53:42 +0900

    DFS_BFS - 자료구조 - 스택 - 예제

--- hello.py
+++ hello.py
@@ -1,66 +1,14 @@
-# N, M을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
+stack = []
 
-# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
-d = [[0] * m for _ in range(n)]  # 리스트 컴프리헨션 문법 이용해서 2차원 리스트 초기화 진행함
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
 
-# 현재 캐릭터의 x 좌표, y 좌표, 방향을 입력받기
-x, y, direction = map(int, input().split())
-d[x][y] = 1  # 현재 좌표 방문 처리
-
-# 전체 맵 정보를 입력받기
-array = []
-for i in range(n):
-    array.append(list(map(int, input().split())))
-
-# 북, 동, 남, 서 방향 정의
-'''
-일반적으로 방향을 설정해서 이동하는 문제 유형에서는 dx, dy라는 별도의 리스트를 만들어 방향을 정하는 것이 효과적이다.
-이처럼 코드를 작성하면 반복문을 이용하여 모든 방향을 차례대로 확인할 수 있다는 점에서 유용하다.
-'''
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-
-# 왼쪽으로 회전
-def turn_left():
-    global direction
-    direction -= 1
-    if direction == -1:
-        direction = 3
-
-
-# 시뮬레이션 시작
-count = 1
-turn_time = 0
-while True:
-    # 왼쪽으로 회전
-    turn_left()
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    # 회전한 이후 정면에 가보지 않은 칸이 존재하고 바다가 아닌 경우 이동
-    if d[nx][ny] == 0 and array[nx][ny] == 0:
-        d[nx][ny] = 1
-        x = nx
-        y = ny
-        count += 1
-        print('x, y: ', x, y)
-        turn_time = 0
-        continue
-    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
-    else:
-        turn_time += 1
-    # 네 방향 모두 갈 수 없는 경우
-    if turn_time == 4:
-        nx = x - dx[direction]
-        ny = y - dy[direction]
-        # 뒤로 갈 수 있다면 이동하기
-        if array[nx][ny] == 0:
-            x = nx
-            y = ny
-            turn_time = 0
-        else:
-            break
-
-# 정답 출력
-print(count)
+print(stack)  # 최하단 원소부터 출력
+print(stack[::-1])  # 최상단 원소부터 출력
