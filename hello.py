dda4ae404e17beea7aac221538798fc1db185576
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 14:38:33 +0900

    DFS_BFS - 탐색 알고리즘 DFS/BFS - 그래프 - 인접 행렬 방식 예제

--- hello.py
+++ hello.py
@@ -1,20 +1,10 @@
-# 반복적으로 구한 n!
-def factorial_iterative(n):
-    result = 1
-    # 1부터 n까지의 수를 차례대로 곱하기
-    for i in range(1, n + 1):
-        result *= i
-    return result
+INF = 9999999999  # 무한의 비용 선언
 
+# 2차원 리스트를 이용해 인접 행렬 표현
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
 
-# 재귀적으로 구현한 n!
-def factorial_recursive(n):
-    if n <= 1:  # n이 1 이하인 경우 1을 반환
-        return 1
-    # n! = n * (n-1)!를 그대로 코드로 작성하기
-    return n * factorial_iterative(n - 1)
-
-
-# 각각의 방식으로 구현한 n! 출력(n=10)
-print('반복적으로 구현: ', factorial_iterative(5))
-print('재귀적으로 구현: ', factorial_recursive(5))
+print(graph)
