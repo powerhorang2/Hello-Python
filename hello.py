fbbb638f78d7968f1626fa1501e8bd9c2216de3c
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 13:58:25 +0900

    DFS_BFS - 자료구조 - 큐 - 예제

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
-stack = []
+from collections import deque
+
+# 큐(Queue) 구현을 위해 deque 라이브러리 사용
+queue = deque()
 
 # 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
-stack.append(5)
-stack.append(2)
-stack.append(3)
-stack.append(7)
-stack.pop()
-stack.append(1)
-stack.append(4)
-stack.pop()
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
 
-print(stack)  # 최하단 원소부터 출력
-print(stack[::-1])  # 최상단 원소부터 출력
+print(queue)  # 먼저 들어온 순서대로 출력
+queue.reverse()  # 다음 출력을 위해 역순으로 바꾸기
+print(queue)
