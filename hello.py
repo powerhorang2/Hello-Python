357f2fbc4cf5b87e215660fb378e7583309170ae
powerhorang2 <tjrdbqls3@naver.com>
2022-05-27 14:19:04 +0900

    DFS_BFS - 팩토리얼 - 2가지 방식으로 구현한 팩토리얼 예제

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-def recursive_function():
-    print('재귀 함수를 호출합니다.')
-    recursive_function()
+# 반복적으로 구한 n!
+def factorial_iterative(n):
+    result = 1
+    # 1부터 n까지의 수를 차례대로 곱하기
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
 
-recursive_function()
+# 재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n <= 1:  # n이 1 이하인 경우 1을 반환
+        return 1
+    # n! = n * (n-1)!를 그대로 코드로 작성하기
+    return n * factorial_iterative(n - 1)
+
+
+# 각각의 방식으로 구현한 n! 출력(n=10)
+print('반복적으로 구현: ', factorial_iterative(5))
+print('재귀적으로 구현: ', factorial_recursive(5))
