c8a3488f7c71466c60db171553d122396a538ec6
powerhorang2 <tjrdbqls3@naver.com>
2022-05-28 17:24:36 +0900

    DFS_BFS - BFS - 미로탈출 - 예제 풀이

--- hello.py
+++ hello.py
@@ -1,41 +1,38 @@
-# N, M을 공백으로 구분하여 입력받기
+from collections import deque
+
 n, m = map(int, input().split())
 
-# 2차원 리스트의 맵 정보 입력받기
-graph = []
+miro = []
+
 for i in range(n):
-    graph.append(list(map(int, input())))
+    miro.append(list(map(int, input())))
+
+# 방향 (좌, 우, 상, 하)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        print('(', x, ', ', y, ') 는 범위를 벗어남')
-        return False
-    # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] == 0:
-        print('(',x,', ',y,') 노드 방문')
-        # 해당 노드 방문 처리
-        graph[x][y] = 1
-        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
-        dfs(x - 1, y)
-        dfs(x, y - 1)
-        dfs(x + 1, y)
-        dfs(x, y + 1)
-        return True
-    print('(', x, ', ', y, ') 는 방문했던 노드')
-    return False
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
 
-# 모든 노드(위치)에 대하여 음료수 채우기
-result = 0
-for i in range(n):
-    for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            print('=====================')
-            print('음료수 채운 노드 좌표: (', i, ', ', j, ')',)
-            print('=====================')
-            result += 1
+    while queue:
+        x, y = queue.popleft()
+        for j in range(4):
+            nx = x + dx[j]
+            ny = y + dy[j]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if miro[nx][ny] == 0:
+                continue
+            if miro[nx][ny] == 1:
+                miro[nx][ny] = miro[x][y] + 1
+                queue.append((nx, ny))
 
-print(result)   # 정답 출력
+    return miro[n - 1][m - 1]
+
+
+print(bfs(0, 0))
+
+for j in range(n):
+    print(miro[j])
