b910778f83a54e3901e1eaf0d81b1fa7ad30855e
powerhorang2 <tjrdbqls3@naver.com>
2022-05-31 15:41:21 +0900

    백준 - 코딩테스트 - DFS와 BFS - 문제 풀이 - 채점 완료

--- hello.py
+++ hello.py
@@ -2,13 +2,39 @@ from collections import deque
 
 n, m, start = map(int, input().split())
 
-graph = []
+graph = [[] for _ in range(n + 1)]
 
-for _ in m:
-    graph.append(list(map(int, input().split())))
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-dfs_visited = [False] * n
+dfs_visited = [False] * (n + 1)
+bfs_visited = [False] * (n + 1)
+
+for i in range(n + 1):
+    graph[i].sort()
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    print(start, end=' ')
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
 
-def dfs(graph, start):
-    dfs_visited[start - 1] = True
+dfs(graph, start, dfs_visited)
+print()
+bfs(graph, start, bfs_visited)
+
